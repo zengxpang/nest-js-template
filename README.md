1f14beeddeb4c066e90179761cc2235e0773c1fe
zengxpang <zengxpang@163.com>
2024-12-02 11:56:25 +0800

    feat: 初始化提交

---
+++ README.md
@@ -0,0 +1,37 @@
+#### 项目说明
+
+本项目是一个基于`NestJs`的模板项目。
+
+
+#### 运行项目的环境准备
+全局安装`nest-cli`,推荐使用`pnpm`安装。本项目`@nestjs/cli`版本为10.4.5，
+并且使用`pnpm`作为包管理工具。
+```bash
+pnpm install -g @nestjs/cli
+```
+
+#### 项目启动
+**安装依赖**
+```bash
+pnpm install
+```
+
+**启动项目**
+```bash
+pnpm run start:dev 
+```
+
+**打包项目**
+```bash
+pnpm run build
+```
+
+**访问项目**
+```bash
+http://localhost:8080
+```
+
+#### 知识储备
+- [NestJs官网](https://docs.nestjs.com/)
+- [NestJs中文网](https://nestjs.inode.club/)
+- [NestJs的依赖注入和控制反转]()
---
+++ src/app.module.ts
@@ -0,0 +1,63 @@
+import * as path from 'path';
+import * as winston from 'winston';
+
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { utilities, WinstonModule } from 'nest-winston';
+import 'winston-daily-rotate-file';
+
+import { CustomExceptionFilter } from './custom-exception.filter';
+import { FormatResponseInterceptor } from './format-response.interceptor';
+import { InvokeRecordInterceptor } from './invoke-record.interceptor';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [path.join(__dirname, '.env')],
+    }),
+    WinstonModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        level: 'debug',
+        transports: [
+          new winston.transports.DailyRotateFile({
+            level: configService.get('WINSTON_LOG_LEVEL'),
+            dirname: configService.get('WINSTON_LOG_DIRNAME'),
+            filename: configService.get('WINSTON_LOG_FILENAME'),
+            datePattern: configService.get('WINSTON_LOG_DATE_PATTERN'),
+            maxSize: configService.get('WINSTON_LOG_MAX_SIZE'),
+          }),
+          new winston.transports.Console({
+            format: winston.format.combine(
+              winston.format.timestamp(),
+              utilities.format.nestLike(),
+            ),
+          }),
+        ],
+      }),
+      inject: [ConfigService],
+    }),
+    CatModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: FormatResponseInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: InvokeRecordInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: CustomExceptionFilter,
+    },
+  ],
+})
+export class AppModule {}
